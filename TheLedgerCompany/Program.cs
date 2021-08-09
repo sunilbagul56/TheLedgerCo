0c373afe4a00699b8d8cd2176be8bfbd2ac77305
Sunil Bagul <sunilbagul56@gmail.com>
2021-08-09 20:23:23 +0530

    code changes

---
+++ TheLedgerCompany/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TheLedgerCompany.Command;
+using TheLedgerCompany.Repository;
+
+namespace TheLedgerCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get loan command details from file
+            var fileProcessor = new FileProcessor();
+            string fileName = "TheLedgerCo-Sample.txt";
+            //string fileName = "TheLedgerCo-Sample-1.txt";
+
+            //Get text file from currect directory
+            //var path = System.Environment.CurrentDirectory;
+
+            string[] commands = fileProcessor.ProcessInputFile(fileName);
+
+            //Process all the commands
+            var dataRepository = new DataRepository();
+            Console.WriteLine("-----------------------------THE LEDGER COMPANY-------------------------------");
+            Console.WriteLine("BankName \t BorroweName \t TotalAmountPaid \t NewTenureInMonths");
+            Console.WriteLine("------------------------------------------------------------------------------");
+
+            var commandInvoker = new CommandInvoker(new LoadCommand(dataRepository), new BalanceCommand(dataRepository), new PaymentCommand(dataRepository));
+            commandInvoker.InvokeCommand(commands);
+        }
+    }
+}
