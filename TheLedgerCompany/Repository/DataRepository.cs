6644f4042ccfa78d428a438f51dbbee6e8cef80d
Sunil Bagul <sunilbagul56@gmail.com>
2021-08-09 20:32:07 +0530

    code changes for repository

---
+++ TheLedgerCompany/Repository/DataRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using TheLedgerCompany.Contract;
+using TheLedgerCompany.Model;
+
+namespace TheLedgerCompany.Repository
+{
+    public class DataRepository : IDataRepository
+    {
+        public Dictionary<string, Loan> DataContext { get; set; }
+
+        public DataRepository()
+        {
+            DataContext = new Dictionary<string, Loan>();
+        }
+
+        public string GetIdentifier(string bankName, string borrowerName)
+        {
+            bankName.EnsureNotNullOrEmpty(nameof(bankName));
+            borrowerName.EnsureNotNullOrEmpty(nameof(borrowerName));
+            return $"{bankName}-{borrowerName}";
+        }
+
+        public Loan GetLoanInfo(string identifier)
+        {
+            DataContext.EnsureCollectionNotNullOrEmpty(nameof(DataContext));
+
+            if (DataContext.ContainsKey(identifier))
+                return DataContext[identifier];
+            else
+                throw new KeyNotFoundException($"The given loan account {identifier} not found");
+        }
+
+        public void AddLoan(string identifier, Loan loan)
+        {
+            DataContext.Add(identifier, loan);
+        }
+    }
+}
---
+++ TheLedgerCompany/Translator/LoadCommandRequestTranslator.cs
@@ -0,0 +1,19 @@
+﻿using TheLedgerCompany.Model;
+
+namespace TheLedgerCompany.Translator
+{
+    public static class LoadCommandRequestTranslator
+    {
+        public static Loan TranslateToDataContext(this LoadCommandRequest loadCommandRequest)
+        {
+            return new Loan()
+            {
+                BankName = loadCommandRequest.BankName,
+                BorrowerName = loadCommandRequest.BorrowerName,
+                Tenure = loadCommandRequest.NoOfYears,
+                RateOfInterest = loadCommandRequest.RateOfInterest,
+                PrincipalAmount = loadCommandRequest.PrincipalAmount
+            };
+        }
+    }
+}
